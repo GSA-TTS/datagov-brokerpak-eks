ef41581adeecdc19d91409c6ea57e3d48fe1a220
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-01-04 11:59:36 -0800

    Replace existing implementation

--- services/terraform/eks-provision.tf
+++ services/terraform/eks-provision.tf
@@ -1,171 +1,195 @@
-output "server" { value = data.aws_eks_cluster.cluster.endpoint }
-output "cluster_ca_certificate" { value = data.aws_eks_cluster.cluster.certificate_authority.0.data }
-output "token" { value = data.aws_eks_cluster_auth.cluster.token }
+output "kubeconfig" { value = data.template_file.kubeconfig.rendered }
 
 locals {
-
+  # TODO: Generate the cluster ID
+  cluster_name = "main"
+  cluster_version = "1.18"
   region = "us-east-1"
-
-  # Options are [default|staging]
-  # env = "${terraform.workspace}"
-  env = "staging"
-
-  cluster_name_map = {
-    default = "datagov-k8s"
-    staging = "datagov-k8s-staging"
-  }
-
-  cluster_name = "${lookup(local.cluster_name_map, local.env)}"
-
-  vpc_cidr_map = {
-    default = "10.0.0.0/16"
-    staging = "172.16.0.0/16"
-  }
-
-  vpc_cidr = "${lookup(local.vpc_cidr_map, local.env)}"
-
-
-  vpc_subnets_map = {
-    default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    staging  = "${cidrsubnets(local.vpc_cidr,4 ,4 ,4 )}"
-  }
- 
-  vpc_subnets= "${lookup(local.vpc_subnets_map, local.env)}"
-
-  base_domain_map = {
-    default = "logtimegames.com"
-    staging = "staging.logtimegames.com"
-  }
-  base_domain = "${lookup(local.base_domain_map, local.env)}"
 }
 
 provider "aws" {
+  # We need at least 3.16.0 because it fixes a problem with creating/deleting
+  # Fargate profiles in parallel. See this issue for more information:
+  # https://github.com/hashicorp/terraform-provider-aws/issues/13372#issuecomment-729689441
+  version = "~> 3.16.0"
   region = local.region
-  profile = "terraform-operator"
-  version = "~> 2.67"
 }
 
-data "aws_availability_zones" "available" {
-  
-}
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.44.0"
-  name = "vpc-${local.cluster_name}"
-  cidr = "${local.vpc_cidr}" # 192.168 , 172.16
-  azs = data.aws_availability_zones.available.names
-  public_subnets = "${local.vpc_subnets}"
+  source = "git::ssh://git@github.com/FairwindsOps/terraform-vpc.git?ref=v5.0.1"
 
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  aws_region = local.region
+  az_count   = 2
+  aws_azs    = "us-east-1b, us-east-1c"
 
-  tags = {
+  enable_s3_vpc_endpoint = "true"
+  
+  # Tag subnets for use by AWS' load-balancers and the ALB ingress controllers
+  # See https://aws.amazon.com/premiumsupport/knowledge-center/eks-vpc-subnet-discovery/
+  global_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
-
-}
-
-resource "aws_iam_policy" "autoscaler_policy" {
-  name        = "autoscaler"
-  path        = "/"
-  description = "Autoscaler bots are fully allowed to read/run autoscaling groups"
- 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-    "Action": [
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeTags",
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "ec2:DescribeLaunchTemplateVersions"
-    ],
-    "Resource": "*",
-    "Effect": "Allow"
-    }
-  ]
-}
-EOF
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  private_prod_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "12.1.0"
-  # insert the 4 required variables here
-  cluster_name = "${local.cluster_name}"
-  cluster_version = "1.16"
-  subnets = module.vpc.public_subnets
-  vpc_id = module.vpc.vpc_id
-  # map_users = var.map_users
-  # worker nodes
-  worker_groups_launch_template = [
-    {
-      name                 = "worker-group-1"
-      instance_type        = "t3.large"
-      asg_desired_capacity = 2
-      asg_max_size = 5
-      asg_min_size  = 2
-      autoscaling_enabled = true
-      public_ip            = true
-      tag_specifications = {
-        resource_type = "instance"
-        tags = {
-          Name = "k8s.io/cluster-autoscaler/${local.cluster_name}"
-        }
-      }
+  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v13.2.1"
+  cluster_name = local.cluster_name
+  cluster_version = local.cluster_version
+  vpc_id       = module.vpc.aws_vpc_id
+  subnets      = module.vpc.aws_subnet_private_prod_ids
 
-      tag_specifications = {
-        resource_type = "instance"
-        tags = {
-          Name = "k8s.io/cluster-autoscaler/enabled"
-        }
-      }
-    }
-  ]
-  # for cluster-autoscaler (page 153 https://docs.aws.amazon.com/eks/latest/userguide/eks-ug.pdf)
-  workers_additional_policies = [
-    aws_iam_policy.autoscaler_policy.arn
-  ]
-
+  # Look ma, no node_groups!
+  # node_groups = {
+  #   eks_nodes = {
+  #     desired_capacity = 3
+  #     max_capacity     = 3
+  #     min_capaicty     = 3
+  #     instance_type = "t2.small"
+  #   }
+  # }
+  manage_aws_auth = false
 }
 
-data "aws_eks_cluster" "cluster" {
+data "aws_eks_cluster" "main" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster_auth" "main" {
   name = module.eks.cluster_id
+}
+
+resource "aws_iam_role" "iam_role_fargate" {
+  name = "eks-fargate-profile"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.iam_role_fargate.name
+}
+
+resource "aws_eks_fargate_profile" "default_namespaces" {
+  depends_on = [module.eks]
+  cluster_name           = data.aws_eks_cluster.main.name
+  fargate_profile_name   = "default_namespaces"
+  pod_execution_role_arn = aws_iam_role.iam_role_fargate.arn
+  subnet_ids             = module.vpc.aws_subnet_private_prod_ids
+  timeouts {
+    # For reasons unknown, Fargate profiles can take upward of 20 minutes to
+    # delete! I've never seen them go past 30m, though, so this seems OK.
+    delete                 = "30m"
+  }
+  selector {
+    namespace = "default"
+  }
+  selector {
+    namespace = "kube-system"
+  }
+}
+
+# Generate a kubeconfig file for use in provisioners and output
+data "template_file" "kubeconfig" {
+  template = <<EOF
+apiVersion: v1
+kind: Config
+current-context: terraform
+clusters:
+- name: ${data.aws_eks_cluster.main.name}
+  cluster:
+    certificate-authority-data: ${data.aws_eks_cluster.main.certificate_authority.0.data}
+    server: ${data.aws_eks_cluster.main.endpoint}
+contexts:
+- name: terraform
+  context:
+    cluster: ${data.aws_eks_cluster.main.name}
+    user: terraform
+users:
+- name: terraform
+  user:
+    token: ${data.aws_eks_cluster_auth.main.token}
+EOF
+}
+
+# Per AWS docs, you have to patch the coredns deployment to remove the
+# constraint that it wants to run on ec2, then restart it. 
+resource "null_resource" "coredns_patch" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOF
+kubectl --kubeconfig=<(echo '${data.template_file.kubeconfig.rendered}') \
+  patch deployment coredns \
+  --namespace kube-system \
+  --type=json \
+  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations", "value": "eks.amazonaws.com/compute-type"}]'
+EOF
+  }
+}
+
+resource "null_resource" "coredns_restart" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOF
+kubectl --kubeconfig=<(echo '${data.template_file.kubeconfig.rendered}') rollout restart -n kube-system deployment coredns
+EOF
+  }
+  depends_on = [
+    null_resource.coredns_patch,
+    aws_eks_fargate_profile.default_namespaces
+  ]
+}
+
+# We need an OIDC provider for the ALB ingress controller to work
+data "tls_certificate" "main" {
+  url = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+resource "aws_iam_openid_connect_provider" "cluster" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.main.certificates[0].sha1_fingerprint]
+  url = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  alias = "eks"
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
   load_config_file       = false
-  version                = "~> 1.11.1"
 }
 
-# Explicitly create namespaces
-resource "kubernetes_namespace" "apps" {
-  metadata {
-    name = "apps"
-  }
-  depends_on = [
-    module.eks.cluster_id
-  ]
-}
+data "aws_region" "current" {}
 
-resource "kubernetes_namespace" "monitoring" {
-  count = local.env == "default" ? 1 : 0
-  metadata {
-    name = "monitoring"
+# Use a convenient module to install the ALB ingress controller
+module "alb_ingress_controller" {
+  source  = "iplabs/alb-ingress-controller/kubernetes"
+  version = "3.4.0"
+
+  providers = {
+    kubernetes = kubernetes.eks
   }
-  depends_on = [
-    module.eks.cluster_id
-  ]
+
+  k8s_cluster_type = "eks"
+  k8s_namespace    = "kube-system"
+
+  aws_region_name  = data.aws_region.current.name
+  k8s_cluster_name = data.aws_eks_cluster.main.name
+
+  # depends_on = [
+  #   aws_eks_fargate_profile.default_namespaces
+  # ]
 }
