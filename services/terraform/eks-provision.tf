574dba20aefa1782d4b00112acf6b05dbeeeaf7b
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-01-28 21:45:27 -0800

    Get fresh token during initialization of kubernetes and helm providers
    This ensures that when providers are initialized, they're using an unexpired token.
    See documentation: https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs#exec-based-credential-plugins

--- services/terraform/eks-provision.tf
+++ services/terraform/eks-provision.tf
@@ -202,17 +202,25 @@ resource "aws_iam_openid_connect_provider" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
   load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
+    command     = "aws-iam-authenticator"
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
     load_config_file       = false
     config_path            = "./kubeconfig_${module.eks.cluster_id}"
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
+      command     = "aws-iam-authenticator"
+    }
   }
   # Helm 2.0.1 seems to have issues with alias. When alias is removed the helm_release provider working
   # Using Helm < 2.0.1 version seem to solve the issue.
