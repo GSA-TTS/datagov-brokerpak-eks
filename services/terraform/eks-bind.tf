44591f160b1ba818fcf91a473d29998ed34369e0
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-02-01 22:43:28 -0800

    Generate a namespace, then a service account bound to a namespace-admin role there

--- services/terraform/eks-bind.tf
+++ services/terraform/eks-bind.tf
@@ -1,26 +1,108 @@
-variable "kubeconfig" { type = string }
+variable "cluster_id" { type = string }
+variable "name" { 
+  type = string 
+  default = ""
+}
+
+output "namespace" { value = kubernetes_namespace.binding.metadata[0].name }
+
 locals {
-  raw_data     = yamldecode(var.kubeconfig)
-  cluster_name = local.raw_data.clusters[0].name
-}
-output "cluster_ca_certificate" { value = local.raw_data.clusters[0].cluster.certificate-authority-data }
-output "server" { value = local.raw_data.clusters[0].cluster.server }
-output "token" { value = data.aws_eks_cluster_auth.instance.token }
-
-# Get a token for the cluster in question
-data "aws_eks_cluster_auth" "instance" {
-  name  = local.cluster_name
+  name        = var.name != "" ? var.name : "ns-${random_id.name.hex}"
 }
 
-# TODO: 
-# 1) Fire up the kubernetes provider
-# 2) Create a namespace
-# 3) Create a Fargate profile for that namespace
-# 4) Generate a role and secret with access to that namespace only (like in solroperator/operator-bind.tf)
+resource "random_id" "name" {
+  byte_length = 8
+}
 
-# provider "kubernetes" {
-#   host                   = var.server
-#   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-#   token                  = base64decode(var.token)
-#   load_config_file       = false
-# }
+provider "kubernetes" {
+  version = "~> 1.13.3"
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
+    command     = "aws-iam-authenticator"
+  }
+}
+
+resource "kubernetes_namespace" "binding" {
+  metadata {
+    name = local.name
+    annotations = {}
+  }
+}
+
+data "aws_eks_cluster" "main" {
+  name  = var.cluster_id
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name  = var.cluster_id
+}
+
+data "aws_iam_role" "iam_role_fargate" {
+  name = "eks-fargate-profile-${data.aws_eks_cluster.main.name}"
+}
+
+resource "aws_eks_fargate_profile" "binding" {
+  cluster_name           = data.aws_eks_cluster.main.name
+  fargate_profile_name   = kubernetes_namespace.binding.metadata[0].name
+  pod_execution_role_arn = data.aws_iam_role.iam_role_fargate.arn
+  subnet_ids             = data.aws_eks_cluster.main.vpc_config[0].subnet_ids
+  selector {
+    namespace = kubernetes_namespace.binding.metadata[0].name
+  }
+}
+
+# Create a service account for the namespace
+resource "kubernetes_service_account" "namespace_admin" {
+  metadata {
+    name = "admin"
+    namespace     = kubernetes_namespace.binding.metadata[0].name
+  }
+}
+
+
+# A namespace-level admin role
+resource "kubernetes_role" "namespace_admin" {
+  metadata {
+    name        = "namespace-admin"
+    namespace   = kubernetes_namespace.binding.metadata[0].name
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+# Bind the namespace-admin role to the service account
+resource "kubernetes_role_binding" "service_account_role_binding" {
+  metadata {
+    name      = "${kubernetes_service_account.namespace_admin.metadata[0].name}-admin-role-binding"
+    namespace = kubernetes_namespace.binding.metadata[0].name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "namespace-admin"
+  }
+
+  subject {
+    api_group = ""
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.namespace_admin.metadata[0].name
+    namespace = kubernetes_namespace.binding.metadata[0].name
+  }
+}
+
+# Read in the generated (default) secret for the service account
+data "kubernetes_secret" "secret" { 
+  metadata {
+    name = kubernetes_service_account.namespace_admin.default_secret_name
+    namespace = kubernetes_namespace.binding.metadata[0].name
+  }
+}
