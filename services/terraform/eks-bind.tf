d1cd22560ba706bb6691259ec27026163816d245
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-02-03 19:53:12 -0800

    Remove the need for consumers to break key values out of the rendered kubeconfig

--- services/terraform/eks-bind.tf
+++ services/terraform/eks-bind.tf
@@ -4,7 +4,12 @@ variable "name" {
   default = ""
 }
 
+
 output "kubeconfig" { value = data.template_file.kubeconfig.rendered }
+output "server" { value = data.aws_eks_cluster.main.endpoint }
+output "certificate_authority_data" { value = data.aws_eks_cluster.main.certificate_authority[0].data }
+output "token" { value = data.kubernetes_secret.secret.data.token}
+output "namespace" { value = kubernetes_namespace.binding.id}
 
 locals {
   name        = var.name != "" ? var.name : "ns-${random_id.name.hex}"
