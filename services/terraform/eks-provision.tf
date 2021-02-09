99c9b0a1832e082242ee43cb4999a36102392ee3
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-02-08 21:10:29 -0800

    Install zookeeper-operator and solr-operator Helm charts
    This is so the CRDs will be available

--- services/terraform/eks-provision.tf
+++ services/terraform/eks-provision.tf
@@ -327,6 +327,38 @@ module "aws_load_balancer_controller" {
 }
 
 
+# --------------------------------------------------------------------------
+# Install Solr and Zookeeper operators so that the CRDs will be available 
+# --------------------------------------------------------------------------
+resource "helm_release" "zookeeper-operator" {
+  name            = "zookeeper"
+  chart           = "zookeeper-operator"
+  repository      = "https://charts.pravega.io/"
+  namespace       = "kube-system"
+  cleanup_on_fail = "true"
+  atomic          = "true"
+  depends_on = [
+    module.vpc, 
+    null_resource.coredns_restart_on_fargate, 
+  ]
+}
+
+resource "helm_release" "solr-operator" {
+  name            = "solr"
+  chart           = "solr-operator"
+  repository      = "https://apache.github.io/lucene-solr-operator/charts"
+  namespace       = "kube-system"
+  cleanup_on_fail = "true"
+  atomic          = "true"
+
+  # We need to wait until the zookeeper-operator is live
+  # This should sidestep the issue described here:
+  # https://github.com/bloomberg/solr-operator/issues/122
+    depends_on = [
+    helm_release.zookeeper-operator
+  ]
+}
+
 # ---------------------------------------------------------
 # Provision the Ingress Controller using Helm
 # ---------------------------------------------------------
