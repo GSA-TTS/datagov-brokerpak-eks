f218fbb3e96a87f04da3fe18b08348c883c4bf78
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-04-12 21:23:59 -0700

    Trim leftovers and move them out of the way for now
    Having them in a different directory means we can run `terraform init` and test the provision "module" in isolation

--- terraform/leftovers.tf
+++ terraform/leftovers.tf
@@ -17,19 +17,7 @@ variable "GRAFANA_PWD" {
   description = "Password to be used for accessing the Grafana service"
 }
 
-
-
-# === HELM STUFF ===
-
-provider "helm" {
-  version = "~> 1.2.3"
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
-  }
-}
+### METRICS
 
 resource "helm_release" "metrics-server" {
   count     = local.env == "default" ? 1 : 0
@@ -80,58 +68,6 @@ resource "helm_release" "cluster-autoscaler" {
   namespace = "kube-system"
   values = [
     templatefile("./charts/cluster-autoscaler/values.yaml", { aws_region = local.region }),
-  ]
-
-  provisioner "local-exec" {
-    command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
-  }
-
-  depends_on = [
-    module.eks.cluster_id
-  ]
-}
-
-
-# SETUP INGRESS
-
-resource "aws_acm_certificate" "cert" {
-  domain_name = "*.${local.base_domain}"
-  # See https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html#alternative-domains-dns-validation-with-route-53
-  #   subject_alternative_names = [
-  #     "*.${local.cluster_name}.${local.base_domain}",
-  #     "${local.cluster_name}.${local.base_domain}"
-  #   ]
-  validation_method = "DNS"
-}
-resource "aws_route53_zone" "zone" {
-  name = local.base_domain
-}
-
-resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id = aws_route53_zone.zone.zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
-  ttl     = 60
-}
-
-# resource "aws_acm_certificate_validation" "cert" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-# }
-
-resource "helm_release" "ingress" {
-  count = local.env == "default" ? 1 : 0
-  name  = "ingress"
-  chart = "stable/nginx-ingress"
-  # version = "1.40.3"
-  namespace       = "kube-system"
-  cleanup_on_fail = "true"
-  atomic          = "true"
-
-  values = [
-    file("./charts/nginx-ingress/values.yaml"),
-    templatefile("./charts/nginx-ingress/values.${local.env}.yaml", { certificate_arn = aws_acm_certificate.cert.arn }),
   ]
 
   provisioner "local-exec" {
