28c26ee104e846a9749d55cdec0645181c1b951a
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-04-12 20:42:06 -0700

    Split the bind operation into the usual Terraform module structure

---
+++ terraform/bind/outputs.tf
@@ -0,0 +1,5 @@
+output "kubeconfig" { value = data.template_file.kubeconfig.rendered }
+output "server" { value = data.aws_eks_cluster.main.endpoint }
+output "certificate_authority_data" { value = data.aws_eks_cluster.main.certificate_authority[0].data }
+output "token" { value = base64encode(data.kubernetes_secret.secret.data.token) }
+output "namespace" { value = local.namespace }
