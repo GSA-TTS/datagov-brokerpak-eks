19ff6f6891a15f963b190d1e9fede0393b3bedf0
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-06-30 19:59:04 -0700

    Use an ingress-nginx controller that still supports v1beta1 (needed by the solr-operator)
    See https://github.com/apache/solr-operator/issues/277

--- terraform/provision/ingress.tf
+++ terraform/provision/ingress.tf
@@ -34,6 +34,7 @@ resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   chart      = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
+  version    = "3.32.0"
 
   namespace       = "kube-system"
   cleanup_on_fail = "true"
