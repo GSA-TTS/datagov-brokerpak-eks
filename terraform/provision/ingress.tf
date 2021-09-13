3b6a7de01b989a8d74e4cd61c043a613509a280d
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-09-12 20:06:47 -0700

    Upgrade AWS provider to 3.31+ to support DS records

--- terraform/provision/ingress.tf
+++ terraform/provision/ingress.tf
@@ -258,10 +258,10 @@ resource "aws_acm_certificate" "cert" {
 
 # Validate the certificate using DNS method
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
   zone_id = aws_route53_zone.cluster.id
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
   ttl     = 60
 }
 
