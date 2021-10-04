8cd98e8e8cf1d6da2cd2b02a5c6971919f7052e8
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-10-04 00:01:35 -0700

    Remove unused resource

--- terraform/provision/main.tf
+++ terraform/provision/main.tf
@@ -9,25 +9,17 @@
 #   https://medium.com/citihub/a-more-secure-way-to-call-kubectl-from-terraform-1052adf37af8
 
 # Confirm that the necessary CLI binaries are present
-resource "null_resource" "prerequisite_binaries_present" {
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<-EOF
-      which aws-iam-authenticator git helm kubectl
-    EOF
-  }
-}
-
+#
 # If the necessary CLI binaries are not present, then we'll only get partway
 # through provisioning before we are stopped cold as we try to use them,
 # leaving everything in a poor state. We want to check for them as early as we
-# can to avoid that. As this random_id is key to a bunch of other
-# provisioning, we add an explicit dependency here to ensure the check for the
-# binaries happens early.
-resource "random_id" "cluster" {
-  byte_length = 8
-
-  depends_on = [
-    null_resource.prerequisite_binaries_present
-  ]
+# can to avoid that. 
+resource "null_resource" "prerequisite_binaries_present" {
+  triggers = {
+    always_run = timestamp()
+  }  
+  provisioner "local-exec" {
+    interpreter = ["/bin/sh", "-c"]
+    command     = "which aws-iam-authenticator git helm kubectl"
+  }
 }
