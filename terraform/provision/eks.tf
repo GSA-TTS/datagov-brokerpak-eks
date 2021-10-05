b890d8d54637304d4074037cec4f424ca62d236d
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-10-04 19:13:27 -0700

    Go back to managing FargateProfiles explicitly

--- terraform/provision/eks.tf
+++ terraform/provision/eks.tf
@@ -9,29 +9,78 @@ module "eks" {
   # module versions above 14.0.0 do not work with Terraform 0.12, so we're stuck
   # on that version until the cloud-service-broker can use newer versions of
   # Terraform.
-  version                       = "~>14.0"
-  cluster_name                  = local.cluster_name
-  cluster_version               = local.cluster_version
-  vpc_id                        = module.vpc.aws_vpc_id
-  subnets                       = module.vpc.aws_subnet_private_prod_ids
-  cluster_enabled_log_types     = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-  cluster_log_retention_in_days = 180
-  manage_aws_auth               = false
-  write_kubeconfig              = var.write_kubeconfig
-  tags                          = merge(var.labels, { "domain" = local.domain })
-  iam_path                      = "/${replace(local.cluster_name, "-", "")}/"
-  fargate_profiles = {
-    default = {
-      name = "default"
-      namespace = "default"
-    }
-    kubesystem = {
-      name = "kube-system"
-      namespace = "kube-system"
-    }
-  }
+  version                         = "~>14.0"
+  cluster_name                    = local.cluster_name
+  cluster_version                 = local.cluster_version
+  vpc_id                          = module.vpc.aws_vpc_id
+  subnets                         = module.vpc.aws_subnet_private_prod_ids
+  cluster_enabled_log_types       = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  cluster_log_retention_in_days   = 180
+  manage_aws_auth                 = false
+  write_kubeconfig                = var.write_kubeconfig
+  tags                            = merge(var.labels, { "domain" = local.domain })
+  iam_path                        = "/${replace(local.cluster_name, "-", "")}/"
+  create_fargate_pod_execution_role = false
+  # fargate_pod_execution_role_name = aws_iam_role.iam_role_fargate.name
+  # fargate_profiles = {
+  #   default = {
+  #     name      = "default"
+  #     namespace = "default"
+  #   }
+  #   kubesystem = {
+  #     name      = "kube-system"
+  #     namespace = "kube-system"
+  #   }
+  # }
 }
 
+resource "aws_iam_role" "iam_role_fargate" {
+  name = "eks-fargate-profile-${local.cluster_name}"
+  tags = var.labels
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.iam_role_fargate.name
+}
+
+
+# We create Fargate profile(s) that select the requested
+# namespace(s). Fargate profiles are expensive to create and destroy, and there
+# can only be one create or destroy operation in flight at a time. So we want to
+# create as few as possible, and do it sequentially rather than in parallel.
+resource "aws_eks_fargate_profile" "default_namespaces" {
+  cluster_name           = data.aws_eks_cluster.main.name
+  fargate_profile_name   = "default-namespaces-${local.cluster_name}"
+  pod_execution_role_arn = aws_iam_role.iam_role_fargate.arn
+  subnet_ids             = module.vpc.aws_subnet_private_prod_ids
+  tags                   = var.labels
+  timeouts {
+    # For reasons unknown, Fargate profiles can take upward of 20 minutes to
+    # delete! I've never seen them go past 30m, though, so this seems OK.
+    delete = "30m"
+  }
+  selector {
+    namespace = "default"
+  }
+  selector {
+    namespace = "kube-system"
+  }
+
+  # This depends_on ensures that this resource is not provisioned until the
+  # cluster's kube API is available.
+  depends_on = [module.eks.cluster_id]
+}
 
 # Per AWS docs, for a Fargate-only cluster, you have to patch the coredns
 # deployment to remove the constraint that it wants to run on ec2, then restart
@@ -58,13 +107,12 @@ resource "null_resource" "cluster-functional" {
       kubectl --kubeconfig <(echo $KUBECONFIG | base64 -d) rollout status -n kube-system deployment coredns
     EOF
   }
-
-  # This depends_on ensures that this resource is not provisioned until the
-  # cluster's kube API is available, and not until we've verified that
-  # prerequisite binaries are available.
+  # This depends_on ensures that coredns will not be patched and restarted until the
+  # FargateProfile is in place, we've verified that prerequisite
+  # binaries are available.
   depends_on = [
     null_resource.prerequisite_binaries_present,
-    module.eks.cluster_id
+    aws_eks_fargate_profile.default_namespaces,
   ]
 }
 
