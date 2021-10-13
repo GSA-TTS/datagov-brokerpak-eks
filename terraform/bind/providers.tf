3b7f621c5f4e8a5f3f3ce304cf72bd0cc52903c3
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-10-13 13:02:15 -0700

    Update k8s provider in the bind operation

--- terraform/bind/providers.tf
+++ terraform/bind/providers.tf
@@ -1,8 +1,9 @@
 provider "kubernetes" {
-  version                = "~> 1.13.3"
+  version = "~>2.5"
+
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.main.token
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
