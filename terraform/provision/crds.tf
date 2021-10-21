da4c739326c9497d522d75ab4239f644fecf3f6d
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-10-20 20:40:13 -0700

    Disable zookeeper delete hook, make helm_releases parallel

--- terraform/provision/crds.tf
+++ terraform/provision/crds.tf
@@ -4,45 +4,51 @@
 # -----------------------------------------------------------------------------
 
 # We install the zookeeper-operator directly rather than letting the
-# solr-operator do it so that it will register its CRDs as part of the helm
-# install process.
+# solr-operator do it so that it will register and unregister its CRDs as part
+# of the helm install process.
 resource "helm_release" "zookeeper-operator" {
-  name            = "zookeeper"
-  chart           = "zookeeper-operator"
-  repository      = "https://charts.pravega.io/"
-  version         = "0.2.12"
-  namespace       = "kube-system"
-  cleanup_on_fail = "true"
-  atomic          = "true"
+  name             = "zookeeper"
+  chart            = "zookeeper-operator"
+  repository       = "https://charts.pravega.io/"
+  version          = "0.2.12"
+  namespace        = "kube-system"
+  set {
+    # See https://github.com/pravega/zookeeper-operator/issues/324#issuecomment-829267141
+    name  = "hooks.delete"
+    value = "false"
+  }
+
   depends_on = [
     null_resource.cluster-functional,
   ]
 }
 
 # TODO: Figure out how we can non-destructively update the CRDs from the
-# upstream manifest without uninstalling/reinstalling the operator. We might be
-# able do this with a null_resource that triggers on the content of the upstream
-# CRD manifest file changing.
+# upstream manifest without uninstalling/reinstalling the operator. See upgrade
+# notes here:
+# https://artifacthub.io/packages/helm/apache-solr/solr-operator#managing-crds
+#
+# We might be able do this with a null_resource that triggers on the content of
+# the upstream CRD manifest file changing.
 resource "helm_release" "solr-operator" {
   name            = "solr"
   chart           = "solr-operator"
   repository      = "https://solr.apache.org/charts"
   version         = "0.4.0"
   namespace       = "kube-system"
-  cleanup_on_fail = "true"
-  atomic          = "true"
 
-  set {
-    name  = "zookeeper-operator.install"
-    value = "false"
-  }
   set {
     name  = "zookeeper-operator.use"
     value = "true"
   }
 
+  set {
+    name  = "zookeeper-operator.install"
+    value = "false"
+  }
+
   depends_on = [
-    helm_release.zookeeper-operator
+    null_resource.cluster-functional,
   ]
 }
 
