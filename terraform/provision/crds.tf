8087a0ac7bdbce2a962ac5b67145461939dcc182
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-12-22 19:04:28 -0800

    Re-upgrade solr operator (#62)
    * update: aws observability syntax change?
    
    * Update to solr-operator 0.5.0
    
    Co-authored-by: Nicholas Kumia <nicholas.kumia@gsa.gov>

--- terraform/provision/crds.tf
+++ terraform/provision/crds.tf
@@ -34,7 +34,7 @@ resource "helm_release" "solr-operator" {
   name            = "solr"
   chart           = "solr-operator"
   repository      = "https://solr.apache.org/charts"
-  version         = "0.2.8"
+  version         = "0.5.0"
   namespace       = "kube-system"
 
   set {
