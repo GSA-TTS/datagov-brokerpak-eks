794507ebfe6eafc11e073da74aa3e0ff3fb4d7ab
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-02-13 01:43:57 -0800

    update: Use the beta authentication standard

--- terraform/provision/providers.tf
+++ terraform/provision/providers.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.main.token
 
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
     command     = "aws-iam-authenticator"
   }
@@ -29,7 +29,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.main.token
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
       command     = "aws-iam-authenticator"
     }
