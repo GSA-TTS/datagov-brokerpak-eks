2df85d2ce3fd2846efe8d899a1237861a8ac71dc
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-02-12 23:33:22 -0800

    update: Terraform to 1.1.5

--- terraform/bind/providers.tf
+++ terraform/bind/providers.tf
@@ -1,5 +1,4 @@
 provider "kubernetes" {
-  version = "~>2.7"
 
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
