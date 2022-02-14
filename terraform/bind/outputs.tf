6c000543e5ac26ac36c236169fb542a1ed022d9e
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-02-14 15:42:31 -0800

    Note that the token output is sensitive

--- terraform/bind/outputs.tf
+++ terraform/bind/outputs.tf
@@ -1,5 +1,8 @@
 output "kubeconfig" { value = data.template_file.kubeconfig.rendered }
 output "server" { value = data.aws_eks_cluster.main.endpoint }
 output "certificate_authority_data" { value = data.aws_eks_cluster.main.certificate_authority[0].data }
-output "token" { value = base64encode(data.kubernetes_secret.secret.data.token) }
+output "token" {
+  sensitive = true
+  value     = base64encode(data.kubernetes_secret.secret.data.token)
+}
 output "namespace" { value = local.namespace }
