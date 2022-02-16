5fb098a291f6601c10dcb35dd6ab940941900116
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-02-16 16:13:37 -0500

    new: add dependency for sc on cluster-functional
    In case kubenetes tries to create the storage class before the cluster is actually functional (i.e. node groups coming up or some other race condition), wait until we know the cluster is more ready

--- terraform/provision/persistent-storage.tf
+++ terraform/provision/persistent-storage.tf
@@ -218,4 +218,7 @@ resource "kubernetes_storage_class" "ebs-sc" {
   # https://docs.aws.amazon.com/eks/latest/userguide/storage-classes.html
   storage_provisioner    = "kubernetes.io/aws-ebs"
   allow_volume_expansion = true
+  depends_on = [
+    null_resource.cluster-functional
+  ]
 }
