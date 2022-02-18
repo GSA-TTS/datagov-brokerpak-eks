61110d3897add9878eee9068d507a67ccf8c7005
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-02-17 20:24:02 -0500

    lint/guide: cleanup printing/print new things
    Fix spacing; don't print a million lines when provisioning/deprovisioning (summarize in one line); print the last description even if it succeeded because there might still be a warning or other non-fatal error

--- bin/instance-wait.sh
+++ bin/instance-wait.sh
@@ -3,7 +3,7 @@
 # Simple script to wait for instance $1 to finish the current operation, and
 # report the result. See OSBAPI spec here:
 #   https://github.com/openservicebrokerapi/servicebroker/blob/master/spec.md#polling-last-operation-for-service-instances
-# 
+#
 # This script uses the "cloud-service-broker client last" subcommand, and
 # requires that the necessary environment variables are set for accessing the
 # service. See configuration documentation here:
@@ -12,28 +12,32 @@
 set -e
 
 function getLast {
-	/bin/cloud-service-broker client last --instanceid "$1"
+  /bin/cloud-service-broker client last --instanceid "$1"
 }
 
 function waitLast {
-	while true; do 
-		LAST=$(getLast "$1");
-        STATUS=$(echo "$LAST" | jq -r .status_code) 
-        STATE=$(echo "$LAST" | jq -r .response.state) 
-        if [[ $STATUS == "410" ]]; then
-            echo "gone!"
-            exit 0
- 	    elif [[ $STATE == "failed" ]]; then
-            echo "$STATE!"
-            echo "$LAST" | jq -r .response.description
-	    exit 1
-        elif [[ $STATE == "succeeded" ]]; then
-            echo "$STATE!"
-            exit 0
-        fi
-        echo "$STATE... "
-		sleep 10
-	done
+  time=0
+  while true; do
+    LAST=$(getLast "$1");
+    STATUS=$(echo "$LAST" | jq -r .status_code)
+    STATE=$(echo "$LAST" | jq -r .response.state)
+    if [[ $STATUS == "410" ]]; then
+      echo "gone!"
+      exit 0
+    elif [[ $STATE == "failed" ]]; then
+      echo "$STATE!"
+      echo "$LAST" | jq -r .response.description
+      exit 1
+    elif [[ $STATE == "succeeded" ]]; then
+      # Sometimes there is a problem even if the provision succeeded
+      echo "$LAST" | jq -r .response.description
+      echo "$STATE!"
+      exit 0
+    fi
+    echo -ne "\r$STATE...($time seconds)"
+    time=$(($time+10))
+    sleep 10
+  done
 }
 
 waitLast "$1"
