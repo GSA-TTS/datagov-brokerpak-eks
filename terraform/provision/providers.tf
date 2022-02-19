61befa90f29c1c972900810c0c90f6abdac5296a
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-02-19 14:04:39 -0500

    fix: env is an argument, not a block

--- terraform/provision/providers.tf
+++ terraform/provision/providers.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
     command     = "aws-iam-authenticator"
-    env {
+    env         = {
       AWS_PROFILE = "tf"
     }
   }
@@ -35,7 +35,7 @@ provider "helm" {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["token", "--cluster-id", data.aws_eks_cluster.main.id]
       command     = "aws-iam-authenticator"
-      env {
+      env         = {
         AWS_PROFILE = "tf"
       }
     }
