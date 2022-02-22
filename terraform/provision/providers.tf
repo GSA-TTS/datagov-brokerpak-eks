10c8bb712c8e442b9ed5af0fb813572ab42ec105
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-02-22 17:54:03 -0500

    revert: remove aws provider from sub-module
    Fix for.. Error: error configuring Terraform AWS Provider: error validating provider credentials: error calling sts:GetCallerIdentity: InvalidClientTokenId: The security token included in the request is invalid.
    │       status code: 403, request id: 38dd9ba8-75e8-4b9d-93c5-dac8c5a2d534
    │
    │   with module.brokerpak-eks-terraform-provision.provider[registry.terraform.io/hashicorp/aws].dnssec-key-provider,
    │   on .terraform/modules/brokerpak-eks-terraform-provision/terraform/provision/providers.tf line 2, in provider aws:
    │    2: provider aws {

--- terraform/provision/providers.tf
+++ terraform/provision/providers.tf
@@ -1,9 +1,4 @@
 
-provider "aws" {
-  alias      = "dnssec-key-provider"
-  region     = "us-east-1"
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
