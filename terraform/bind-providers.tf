11ee0797fc64dee7a84845ff0dcf4ceb7a4cae49
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-02-28 15:36:23 -0800

    cleanup: remove outdated comments

--- terraform/bind-providers.tf
+++ terraform/bind-providers.tf
@@ -1,16 +1,3 @@
-# resource "local_file" "bind-kubeconfig" {
-#   sensitive_content = module.provision.admin_kubeconfig
-#   filename          = "./kubeconfig-for-bind"
-#   file_permission   = "0600"
-#   depends_on = [
-#     module.provision
-#   ]
-# }
-# provider "kubernetes" {
-#   alias = "bind"
-#   config_path = local_file.bind-kubeconfig.filename
-# }
-
 provider "kubernetes" {
   alias = "bind"
   host                   = module.provision.host
