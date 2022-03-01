021faff403e1777ac1661ae3990c252f60e99f3b
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-02-28 18:07:36 -0800

    fix: confirm we do want to output the sensitive token value

--- terraform/modules/provision/outputs.tf
+++ terraform/modules/provision/outputs.tf
@@ -5,4 +5,5 @@ output "cluster-id" { value = data.aws_eks_cluster.main.id }
 output "token" { 
   value = data.kubernetes_secret.secret.data.token
   description = "A cluster-admin token for use in constructing your own kubernetes configuration. NOTE: Do _not_ use this token when configuring the required_provider or you'll get a dependency cycle. Instead use exec with the same AWS credentials that were used for the required_providers aws provider."
+  sensitive = true
 }
