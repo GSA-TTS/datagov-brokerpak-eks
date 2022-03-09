7bf23955589da2fb8e9afc6fa3670c21ecfaa0c0
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-09 00:43:27 -0800

    refactor: persistent-storage.tf into provision-aws and provision-k8s sections

--- terraform/modules/provision-aws/outputs.tf
+++ terraform/modules/provision-aws/outputs.tf
@@ -2,5 +2,6 @@ output "domain_name" { value = local.domain }
 output "server" { value = data.aws_eks_cluster.main.endpoint }
 output "certificate_authority_data" { value = data.aws_eks_cluster.main.certificate_authority[0].data }
 output "cluster-id" { value = data.aws_eks_cluster.main.id }
+output "persistent_storage_key_id" { value = aws_kms_key.ebs-key.key_id }
 output "zone_id" { value = aws_route53_zone.cluster.zone_id }
 output "zone_role_arn" { value = aws_iam_role.external_dns.arn}
