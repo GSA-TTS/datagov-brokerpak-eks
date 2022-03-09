f77d57c8cf66f6373c3110e4fdf740c4c7565b2c
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-08 21:31:58 -0800

    refactor: k8s admin account will be created in a separate module from AWS resources

---
+++ terraform/modules/post-provision/outputs.tf
@@ -0,0 +1,5 @@
+output "token" {
+  value       = data.kubernetes_secret.secret.data.token
+  description = "A cluster-admin token for use in constructing your own kubernetes configuration. NOTE: Do _not_ use this token when configuring the required_provider or you'll get a dependency cycle. Instead use exec with the same AWS credentials that were used for the required_providers aws provider."
+  sensitive   = true
+}
--- terraform/modules/post-provision/variables.tf
+++ terraform/modules/post-provision/variables.tf
@@ -1,2 +1,19 @@
+# The name of the k8s instance we're setting up
+variable "instance_name" {
+  type    = string
+  default = ""
+}
+
+# The server for the k8s API
+variable "server" {
+  type = string
+}
+
+# The certificate_authority_data for the k8s instance
+variable "certificate_authority_data" {
+  type = string
+}
+
 # Information about the current region
 data "aws_region" "current" {}
+
