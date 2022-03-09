3db20b72eae5e6ccd02495e2a55828801a123d9c
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-09 00:22:53 -0800

    refactor: rename module.provision -> module.provision-aws to match the module source

--- terraform/bind-providers.tf
+++ terraform/bind-providers.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
   alias                  = "bind"
-  host                   = module.provision.server
-  cluster_ca_certificate = base64decode(module.provision.certificate_authority_data)
+  host                   = module.provision-aws.server
+  cluster_ca_certificate = base64decode(module.provision-aws.certificate_authority_data)
   token                  = module.provision-k8s.token
 }
