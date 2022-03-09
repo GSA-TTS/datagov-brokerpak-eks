dfbb2495d560559b5c640a9406f6ec0b1c5ffed2
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-08 23:23:36 -0800

    refactor: split external-dns into aws- and k8s-specific submodules

--- terraform/modules/post-provision/variables.tf
+++ terraform/modules/post-provision/variables.tf
@@ -1,3 +1,19 @@
+# The domain suffix to use for all DNS entries
+variable "domain" {
+  type = string
+}
+
+# The ID of the Route53 zone where external DNS records for ingresses should be
+# maintained
+variable "zone_id" {
+  type = string
+}
+
+# The ARN of an IAM role that is able to manipulate records in the Route53 zone_id.
+variable "zone_role_arn" {
+  type = string
+}
+
 # The name of the k8s instance we're setting up
 variable "instance_name" {
   type    = string
