499cad9d35ddbb167b0cfa777da4a2fa858423fc
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-16 14:52:51 -0700

    fix: xlarge instances aren't available in us-west-2d, so skip that AZ

--- terraform/modules/provision-aws/vpc.tf
+++ terraform/modules/provision-aws/vpc.tf
@@ -15,8 +15,8 @@ module "vpc" {
   azs             = data.aws_availability_zones.available.names
   # These subnets represent AZs us-west-2a, us-west-2b, and us-west-2c
   # This gives us 8187 IP addresses that can be given to nodes and (via the VPC-CNI add-on) pods.
-  private_subnets = ["10.31.0.0/19", "10.31.32.0/19", "10.31.64.0/19", "10.31.96.0/19"]
-  public_subnets  = ["10.31.128.0/19", "10.31.160.0/19", "10.31.192.0/19", "10.31.224.0/19"]
+  private_subnets = ["10.31.0.0/19", "10.31.32.0/19", "10.31.64.0/19"]
+  public_subnets  = ["10.31.128.0/19", "10.31.160.0/19", "10.31.192.0/19"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
