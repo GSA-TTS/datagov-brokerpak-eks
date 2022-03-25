40727a668b3464789c2ecd87b8e8651561112d54
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-03-25 11:16:05 -0700

    refactor: account for planned extensions

--- terraform/modules/provision-k8s/k8s-network-policy.tf
+++ terraform/modules/provision-k8s/k8s-network-policy.tf
@@ -8,7 +8,22 @@ resource "kubernetes_namespace" "calico-system" {
   }
 }
 
+locals {
+  # In future, we will extend this list
+  #   1. To account for additional namespaces having been provisioned
+  #     See https://github.com/GSA/data.gov/issues/3013
+  #   2. To lock down "admin" namespaces and then selectively add allow policies
+  #     For example, add default-deny in kube-system, then add a separate explicit
+  #     allow policy for things that need to use AWS APIs like the 
+  #     aws-load-balancer-controller
+  # ...but for now, we only care about the place where consumers will put their 
+  # workloads.
+  namespace_list = ["default"]
+}
+
 resource "kubernetes_network_policy" "default-deny" {
+  for_each = toset(local.namespace_list)
+  
   metadata {
     name      = "default-deny-egress"
     namespace = "default"
