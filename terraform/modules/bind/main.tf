006f2e1bd8a2cc5d295e92b1884f2da603d02825
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-03-28 16:14:54 -0400

    revert: the default admin ClusterRole isn't enough
    The default admin role does not give solr-operator enough permissions to create solr nodes

--- terraform/modules/bind/main.tf
+++ terraform/modules/bind/main.tf
@@ -21,8 +21,8 @@ resource "kubernetes_role_binding" "binding" {
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "admin"
+    kind      = "Role"
+    name      = "namespace-admin"
   }
 
   subject {
