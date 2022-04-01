1b4e80813ed66835865801a05d5ad2fba62d3ab9
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-04-01 16:33:11 -0400

    update: local_file to local_sensitive_file

--- terraform/modules/provision-aws/eks.tf
+++ terraform/modules/provision-aws/eks.tf
@@ -307,10 +307,10 @@ data "template_file" "kubeconfig" {
   EOF
 }
 
-resource "local_file" "kubeconfig" {
+resource "local_sensitive_file" "kubeconfig" {
   # Only create the file if requested; it's not needed by provisioners
   count             = var.write_kubeconfig ? 1 : 0
-  sensitive_content = data.template_file.kubeconfig.rendered
+  content           = data.template_file.kubeconfig.rendered
   filename          = local.kubeconfig_name
   file_permission   = "0600"
 }
