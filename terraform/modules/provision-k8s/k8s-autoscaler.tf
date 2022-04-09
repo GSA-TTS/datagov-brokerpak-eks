df31d3d26eae60baf3246e387e92704627a0494f
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2022-04-08 22:49:20 -0700

    fix: use CRDs only after they're available
    See https://github.com/GSA/datagov-brokerpak-eks/pull/67#issuecomment-1093641392 for explanation

--- terraform/modules/provision-k8s/k8s-autoscaler.tf
+++ terraform/modules/provision-k8s/k8s-autoscaler.tf
@@ -1,35 +1,32 @@
-
-resource "kubernetes_manifest" "autoscaler-provisioner" {
-  manifest = {
-    "apiVersion" = "karpenter.sh/v1alpha5"
-    "kind" = "Provisioner"
-    "metadata" = {
-      "name" = "default-provisioner"
-    }
-    "spec" = {
-      "limits" = {
-        "resources" = {
-          "cpu" = 1000
-        }
-      }
-      "provider" = {
-        "launchTemplate" = local.launch_template_name
-        "subnetSelector" = {
-          "karpenter.sh/discovery" = local.cluster_name
-        }
-      }
-      "requirements" = [
-        {
-          "key" = "karpenter.sh/capacity-type"
-          "operator" = "In"
-          "values" = [
-            "on-demand",
-          ]
-        },
-      ]
-      "ttlSecondsAfterEmpty" = 30
-    }
-  }
+# This is a workaround to use CRDs in the same Terraform pass as they are made
+# available. Explanation:
+# https://github.com/GSA/datagov-brokerpak-eks/pull/67#issuecomment-1093641392
+resource "helm_release" "autoscaler-provisioner" {
+  name       = "autocaler-provisioner"
+  repository = "https://charts.itscontained.io"
+  chart      = "raw"
+  version    = "0.2.5"
+  values = [
+    <<-EOF
+    apiVersion: karpenter.sh/v1alpha5
+    kind: Provisioner
+    metadata:
+      name: default
+    spec:
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+      limits:
+        resources:
+          cpu: 1000
+      provider:
+        launchTemplate: <launch-template-name>
+        subnetSelector:
+          karpenter.sh/discovery: <cluster-name>
+      ttlSecondsAfterEmpty: 30
+    EOF
+  ]
   depends_on = [
     null_resource.cluster-functional,
     helm_release.karpenter
