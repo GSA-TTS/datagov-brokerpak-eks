4587ed5af45711ec0f7c64bdde75e82237cfaee8
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-04-26 16:28:22 -0400

    new: ensure EBS volumes created are compatible with topology

--- terraform/modules/provision-k8s/k8s-persistent-storage.tf
+++ terraform/modules/provision-k8s/k8s-persistent-storage.tf
@@ -11,4 +11,18 @@ resource "kubernetes_storage_class" "ebs-sc" {
   # https://docs.aws.amazon.com/eks/latest/userguide/storage-classes.html
   storage_provisioner    = "kubernetes.io/aws-ebs"
   allow_volume_expansion = true
+
+  # Ensure volumes are created in the correct topology (specifically availability zone)
+  # https://kubernetes.io/docs/concepts/storage/storage-classes/#volume-binding-mode
+  volume_binding_mode    = "WaitForFirstConsumer"
+
+  allowed_topologies {
+    dynamic "match_label_expressions" {
+      for_each = var.single_az ? [1] : []
+      content {
+        key    = "topology.ebs.csi.aws.com/zone"
+        values = ["${var.region}a"]
+      }
+    }
+  }
 }
