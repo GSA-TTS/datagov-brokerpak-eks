36bfb9eb0008e8180644ce137fdfbc5f0e35cc3b
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-05-05 10:46:57 -0400

    new: update terraform.tfvars backend secrets
    Use the s3creds.sh to optionally update the terraform.tfvars directly if a filename is provided

--- docs/s3creds.sh
+++ docs/s3creds.sh
@@ -16,3 +16,13 @@ echo export AWS_ACCESS_KEY_ID=`echo "${S3_CREDENTIALS}" | jq -r .credentials.acc
 echo export AWS_SECRET_ACCESS_KEY=`echo "${S3_CREDENTIALS}" | jq -r .credentials.secret_access_key`
 echo export BUCKET_NAME=`echo "${S3_CREDENTIALS}" | jq -r .credentials.bucket`
 echo export AWS_DEFAULT_REGION=`echo "${S3_CREDENTIALS}" | jq -r '.credentials.region'`
+
+# To directly update the terraform.tfvars with these values, please provide
+# the name of the file to udpate (e.g. terraform/modules/provision-aws/terraform.tfvars)
+
+if [ ${3} ]; then
+  sed -i "s/s3_bucket_name.*/s3_bucket_name = `echo "${S3_CREDENTIALS}" | jq -r .credentials.bucket`/" $3
+  sed -i "s/s3_region.*/s3_region = `echo "${S3_CREDENTIALS}" | jq -r '.credentials.region'`/" $3
+  sed -i "s/s3_aws_access_key_id.*/s3_aws_access_key_id = `echo "${S3_CREDENTIALS}" | jq -r .credentials.access_key_id`/" $3
+  sed -i "s/s3_aws_secret_access_key.*/s3_aws_secret_access_key = `echo "${S3_CREDENTIALS}" | jq -r .credentials.secret_access_key`/" $3
+fi
