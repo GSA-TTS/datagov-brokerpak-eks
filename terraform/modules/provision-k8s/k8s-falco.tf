d0368b58ebcb2a41ffa99deacc08cfa5e5db17ca
Fuhu Xia <fxia@reisystems.com>
2022-05-12 11:14:58 -0400

    create a falco namespace

--- terraform/modules/provision-k8s/k8s-falco.tf
+++ terraform/modules/provision-k8s/k8s-falco.tf
@@ -1,17 +1,26 @@
+# Creating namespace for falco.
+# So that the default-deny-egress network policy does not affect falco pods.
+resource "kubernetes_namespace" "falco" {
+  metadata {
+    name = "falco"
+  }
+}
+
 resource "helm_release" "falco" {
   name       = "falco"
   chart      = "falco"
   repository = "https://falcosecurity.github.io/charts"
   version    = "1.18.3"
 
-  namespace       = "default"
+  namespace       = kubernetes_namespace.falco.metadata[0].name
   cleanup_on_fail = "true"
   timeout         = 600
 
   dynamic "set" {
     for_each = {
-      "falcosidekick.enabled"                 = true,
-      "falcosidekick.config.slack.webhookurl" = var.slack_webhookurl,
+      "falcosidekick.enabled"                      = true,
+      "falcosidekick.config.slack.webhookurl"      = var.slack_webhookurl,
+      "falcosidekick.config.slack.minimumpriority" = "warning",
     }
     content {
       name  = set.key
