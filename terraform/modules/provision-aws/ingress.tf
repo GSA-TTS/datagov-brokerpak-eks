9ccfe7b3d5fa4ab227614c18df3c6e98bdd57fae
Nicholas Kumia <nicholas.kumia@reisystems.com>
2023-10-05 19:59:24 -0400

    update: ingress-nginx helm version
    - ERROR: │ Error: unable to build kubernetes objects from release manifest: resource mapping not found for name: 'ingress-nginx-controller' namespace: 'kube-system' from '': no matches for kind 'HorizontalPodAutoscaler' in version 'autoscaling/v2beta2'
    │ ensure CRDs are installed first
    - Solution: took a chance and just updated the chart version

--- terraform/modules/provision-aws/ingress.tf
+++ terraform/modules/provision-aws/ingress.tf
@@ -38,7 +38,7 @@ resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   chart      = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  version    = "4.0.17"
+  version    = "4.8.1"
 
   namespace       = "kube-system"
   cleanup_on_fail = "true"
